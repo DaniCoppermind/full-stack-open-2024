ce88ae693f36eefa8d7f5f0ab9edae7dc715c94c
Daniel García <workspacedaniel2121@gmail.com>
2024-12-07 12:47:13 +0100

    1.1 Done

---
+++ part1/courseInfo/src/App.jsx
@@ -0,0 +1,47 @@
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{props.course}</h1>
+    </header>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Total Exercises: <strong>{props.totalExercises}</strong>
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+      <Header course={course} />
+      <main>
+        <Content part={part1} exercises={exercises1} />
+        <Content part={part2} exercises={exercises2} />
+        <Content part={part3} exercises={exercises3} />
+        <Total totalExercises={exercises1 + exercises2 + exercises3} />
+      </main>
+    </>
+  )
+}
+
+export default App
