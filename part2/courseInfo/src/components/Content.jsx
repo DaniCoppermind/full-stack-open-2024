5d668099763cb6bdd456c9386a92dee9a5301d0f
Daniel García <workspacedaniel2121@gmail.com>
2024-12-11 13:10:31 +0100

    2.2 and 2.3* courseInfo

--- part2/courseInfo/src/components/Content.jsx
+++ part2/courseInfo/src/components/Content.jsx
@@ -1,11 +1,20 @@
 import Part from './Part'
 
 function Content({ parts }) {
+  // const total = parts.map((part) => {
+  //   console.log(part.exercises)
+  // })
+
+  const total = parts.reduce((sum, part) => {
+    return sum + part.exercises
+  }, 0)
+
   return (
     <section>
       {parts.map((part) => (
         <Part key={part.id} name={part.name} exercises={part.exercises} />
       ))}
+      <b>Total of {total} exercises</b>
     </section>
   )
 }
