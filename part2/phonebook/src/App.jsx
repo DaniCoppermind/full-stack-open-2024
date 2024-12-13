cd5e2a8013e3c5d57ec67021a404f27676cbd4d9
Daniel García <workspacedaniel2121@gmail.com>
2024-12-13 18:25:38 +0100

    2.11: La Agenda Telefónica Paso 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-
-const personsData = [
-  { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-]
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState(personsData)
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('') // input name form
   const [newNumber, setNewNumber] = useState('')
   const [searchPerson, setsearchPerson] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }, [])
 
   const handleNameSubmit = (event) => {
     event.preventDefault()
