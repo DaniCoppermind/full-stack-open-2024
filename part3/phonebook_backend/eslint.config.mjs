7a18d0483756af1864a4da75728a4b744545d184
Daniel García <workspacedaniel2121@gmail.com>
2025-01-03 17:47:00 +0100

    eslint config

---
+++ part3/phonebook_backend/eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from 'globals'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'warn',
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+]
