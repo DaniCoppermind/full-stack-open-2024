d4dbc3355b603f86d8b5d7428b2720ef20933100
Daniel García <workspacedaniel2121@gmail.com>
2025-01-10 18:49:35 +0100

    eliminar el campo 'name' del modelo de usuario y controlador; agregar pruebas para la API de usuarios

--- part4/blogList_backend/tests/test_helper.js
+++ part4/blogList_backend/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog.model')
+const User = require('../models/user.model')
 
 const initialBlogs = [
   {
@@ -52,8 +53,14 @@ const blogsInDb = async () => {
   return blogs.map((blog) => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map((u) => u.toJSON())
+}
+
 module.exports = {
   initialBlogs,
   nonExistingId,
   blogsInDb,
+  usersInDb,
 }
