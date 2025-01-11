1e5da3690d8539a9a41dbae6c12f717dcecfe909
Daniel García <workspacedaniel2121@gmail.com>
2025-01-11 15:37:23 +0100

    blogs tienen información de user y viceversa

--- part4/blogList_backend/controllers/blog.controller.js
+++ part4/blogList_backend/controllers/blog.controller.js
@@ -1,13 +1,15 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog.model')
+const User = require('../models/user.model')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
-  const { title, author, url, likes } = request.body
+  const { title, author, url, likes, userId } = request.body
+  const user = await User.findById(userId)
 
   if (!title || !url) {
     return response.status(400).json({ error: 'title and url are required' })
@@ -18,9 +20,13 @@ blogRouter.post('/', async (request, response) => {
     author,
     url,
     likes: likes || 0,
+    user: user.id,
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
