033e21acd7a265d60be699c6a3c3eb06d04357c1
Daniel <workspacedaniel2121@gmail.com>
2025-04-02 20:27:12 +0200

    typescript patients entry

--- part9/patientor/backend/types.ts
+++ part9/patientor/backend/types.ts
@@ -3,3 +3,14 @@ export type DiagnosesEntry = {
   name: string;
   latin?: string
 }
+
+export type PatientsEntry = {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: string;
+  occupation: string;
+}
+
+// type Genre = 'male' | 'female' | 'other'
