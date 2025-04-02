95c6da7c524723e4b73910ac2ea1e740aaee6a54
Daniel <workspacedaniel2121@gmail.com>
2025-04-02 18:35:39 +0200

    Agregar configuración inicial de frontend y backend con TypeScript y Express

---
+++ part9/flight-diary/eslint.config.mjs
@@ -0,0 +1,42 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    ignores: ['build/'], // Ignorar la carpeta build
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json', // Ruta al archivo tsconfig.json
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...eslint.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...tseslint.configs['recommended-requiring-type-checking'].rules,
+      '@typescript-eslint/semi': ['error'],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/restrict-plus-operands': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      'no-case-declarations': 'off',
+    },
+    environment: {
+      browser: true,
+      es6: true,
+      node: true,
+    },
+  },
+];
